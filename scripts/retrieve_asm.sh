d69bb59377a73ad930bdb9eb2bf8a4c996b68742
CorbinBryan <bryan7@wisc.edu>
2026-02-17 12:29:58 -0600

    added a new script to retrieve assemblies and rename them according to the directory in which they were found

--- scripts/retrieve_asm.sh
+++ scripts/retrieve_asm.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+# BASEDIR="$1" 
+
+# for DIR in $(ls ${BASEDIR}); do 
+#     if [[ -d ${DIR} ]]; then
+#         ACC="$DIR"
+#         mv ${BASEDIR}/${DIR}/assembly/contigs.fasta ${BASEDIR}/${ACC}_contigs.fasta
+#         mv ${BASEDIR}/${DIR}/assembly/scaffolds.fasta ${BASEDIR}/${ACC}_scaffolds.fasta
+#     fi 
+# done
+
+#!/bin/bash
+
+BASEDIR="$1" 
+
+for DIR in "${BASEDIR}"/*; do 
+    if [[ -d "$DIR" ]]; then
+        ACC="$(basename "$DIR")"
+        mv "$DIR/assembly/contigs.fasta" "${BASEDIR}/${ACC}_contigs.fasta"
+        mv "$DIR/assembly/scaffolds.fasta" "${BASEDIR}/${ACC}_scaffolds.fasta"
+    fi 
+done
